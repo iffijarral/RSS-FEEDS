134bfeb65c2c5220d3535a15aba8adbe2452a950
iffijarral <iffi_kunjahi@yahoo.com>
2021-09-07 10:35:22 +0200

    initial commit

---
+++ app/backend/auth/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$GLOBALS['config'] = array(
+
+    'app' => array(
+        'name'          => 'Medborgerskabsprøven.dk',
+        'serverKey'     => '5f2b5cdbe5194f10b3241568fe4e2b24',
+        'base_url'      => 'https://medborgerskabsprove.dk',
+    ),
+
+    'mysql' => array(
+        'host'          => 'localhost',
+        'username'      => 'root',
+        'password'      => '',
+        'db_name'        => 'test'
+    ),
+);
